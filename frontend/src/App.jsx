bb8180d3142c6da383ef924f3641e2ffedcce434
Pratham Lohia <121216418+prats-13@users.noreply.github.com>
2025-07-05 13:54:39 +0530

    Created routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
+ 
 const App = () => {
   return (
-    <div>
-      
+    <div className='mx-4 sm:mx-[10%]'>
+      <Routes>
+        <Route path='/' element={ <Home/> } />
+        <Route path='/login' element={ <Login/> } />
+        <Route path='/myprofile' element={ <MyProfile/> } />
+        <Route path='/my-appointments' element={ <MyAppointments/> } />
+        <Route path='/appointment/:docId' element={ <Appointment/> } />
+        <Route path='/doctors' element={ <Doctors/> } />
+        <Route path='/doctors/:speciality' element={ <Doctors/> } />
+        <Route path='/about' element={ <About/> } />
+        <Route path='/contact' element={ <Contact/> } />
+      </Routes>
     </div>
   )
 }
