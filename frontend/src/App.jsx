6c47cd4f77070ddff553238e6e96eb5fc6f772e7
Pratham Lohia <121216418+prats-13@users.noreply.github.com>
2025-07-06 00:26:21 +0530

    mounted nav bar into app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,14 +8,16 @@ import Login from './pages/Login'
 import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
  
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
+      <Navbar />
       <Routes>
         <Route path='/' element={ <Home/> } />
         <Route path='/login' element={ <Login/> } />
-        <Route path='/myprofile' element={ <MyProfile/> } />
+        <Route path='/my-profile' element={ <MyProfile/> } />
         <Route path='/my-appointments' element={ <MyAppointments/> } />
         <Route path='/appointment/:docId' element={ <Appointment/> } />
         <Route path='/doctors' element={ <Doctors/> } />
