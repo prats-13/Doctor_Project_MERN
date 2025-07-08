66d7817d1cced1ab9ba4b24fb855fcb94b920680
Pratham Lohia <121216418+prats-13@users.noreply.github.com>
2025-07-08 19:50:28 +0530

    created the component

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -13,7 +13,7 @@ const TopDoctors = () => {
       <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
       <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 pt-5 px-3 sm:px-0'>
         {doctors.slice(0,10).map((doctor,index)=>(
-          <div onClick={()=>navigate(`/appointment/${doctor._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-200' key={index}>
+          <div onClick={()=>{navigate(`/appointment/${doctor._id}`); scrollTo(0,0)}} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-200' key={index}>
             <img className='bg-blue-50' src={doctor.image} alt="" />
             <div className='p-4'>
               <div className='flex items-center gap-2 text-sm text-center text-green-500'>
