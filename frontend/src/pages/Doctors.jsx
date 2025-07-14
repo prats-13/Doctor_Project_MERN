e32cec0f45ffbc43e1cfa8267a8aabef3ad0b5ea
Pratham Lohia <121216418+prats-13@users.noreply.github.com>
2025-07-14 22:09:16 +0530

    included responsiveness for small devices

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -8,6 +8,7 @@ const Doctors = () => {
   //console.log(speciality);
   const navigate=useNavigate()
   const [filterDoc,setFilterDoc]=useState([])
+  const [showFilter,setShowFilter]=useState(false)
   const {doctors}=useContext(AppContext)
   
   const specialities = [
@@ -37,7 +38,8 @@ const Doctors = () => {
     <div>
       <p className='text-gray-600'>Browse through the doctors specialist.</p>
       <div className='flex flex-col sm:flex-row items:start gap-5 mt-5'>
-        <div className='flex flex-col gap-4 text-sm text-gray-600'>
+        <button className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter?'bg-[#5f6FFF] text-white':''}`} onClick={()=>setShowFilter(prev=>!prev)}>Filters</button>
+        <div className={`flex flex-col gap-4 text-sm text-gray-600 ${showFilter? 'flex': 'hidden sm:flex'}`}>
           {specialities.map((item, index) => (
             <p
               key={index}
