550a6cbeb253a1a48ac67dcdf14a8f0dc79726d3
Pratham Lohia <121216418+prats-13@users.noreply.github.com>
2025-07-16 22:15:05 +0530

    added context providers

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,11 +1,19 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
+import AdminContextProvider from './context/AdminContextProvider.jsx'
+import DoctorContextProvider from './context/DoctorContextProvider.jsx'
+import AppContextProvider from './context/AppContextProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <AdminContextProvider>
+      <DoctorContextProvider>
+        <AppContextProvider>
+          <App />
+        </AppContextProvider>
+      </DoctorContextProvider>
+    </AdminContextProvider>
   </BrowserRouter>,
 )
