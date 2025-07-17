364836556e9e5de980e38df96138525ab919310f
Pratham Lohia <121216418+prats-13@users.noreply.github.com>
2025-07-17 12:00:39 +0530

    mounted navbar for admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,14 +2,16 @@ import React, { useContext } from 'react'
 import Login from './pages/Login'
 import { ToastContainer } from 'react-toastify';
 import { AdminContext } from './context/AdminContext';
+import Navbar from './components/Navbar';
 
 const App = () => {
 
   const {aToken}=useContext(AdminContext)
 
   return aToken ? (
-    <div>
+    <div className='bg-[#F8F9FD]'>
       <ToastContainer/>
+      <Navbar/>
     </div>
   ) : (
     <>
