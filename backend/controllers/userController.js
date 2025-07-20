b423c6746827d2618f99effef51d83ae5ef370ca
Pratham Lohia <121216418+prats-13@users.noreply.github.com>
2025-07-20 21:12:31 +0530

    created register and login controller functions

---
+++ backend/controllers/userController.js
@@ -0,0 +1,88 @@
+import validator from 'validator'
+import bcrypt from 'bcrypt'
+import userModel from '../models/userModel.js'
+import jwt from 'jsonwebtoken'
+
+/* api controller function to register a new user*/
+
+const registerUser=async(req,res)=>{
+  try{
+    const {name,email,password}=req.body
+
+    if(!name || !email || !password){
+      return res.json({success:false,message:"Missing details"})
+    }
+
+    if(!validator.isEmail(email)){
+      return res.json({success:false,message:"Enter a valid email"})
+    }
+
+    if(password.length<8){
+      return res.json({success:false,message:"Password length must be atleast 8"})
+    }
+
+    /* hashing user password */
+    const salt=await bcrypt.genSalt(10)
+    const hashedPassword=await bcrypt.hash(password,salt)
+
+    const userData={
+      name,
+      email,
+      password:hashedPassword
+    }
+
+    const newUser=new userModel(userData)
+    const user=await newUser.save()
+    /* in the user object we get a _id property and by using this we will create a token so user can login on the website */
+
+    const token=jwt.sign({id:user._id},process.env.JWT_SECRET)
+    
+    res.json({success:true,token})
+  }catch(error){
+    console.log(error)
+    res.json({success:false,message:error.message})
+  }
+}
+
+/* API for user login */
+const loginUser=async(req,res)=>{
+  try{
+    const {email,password}=req.body
+    const user=await userModel.findOne({email})
+
+    if(!user){
+      res.json({success:false,message:'User does not exist'})
+    }
+
+    const isMatch=await bcrypt.compare(password,user.password)
+    if(isMatch){
+      const token=jwt.sign({id:user._id},process.env.JWT_SECRET)
+      res.json({success:true,token})
+    }else{
+      res.json({success:false,message:"Invalid credentials"})
+    }
+  }catch(error){
+    console.log(error)
+    res.json({success:false,message:error.message})
+  }
+}
+
+export {registerUser,loginUser}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* login register getprofile update profile book appointment displaying book appointment cancelling appointment payment gateway*/
