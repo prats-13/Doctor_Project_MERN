a8457d751ef877aa50940a395086d91ee19c5497
Pratham Lohia <121216418+prats-13@users.noreply.github.com>
2025-07-20 22:22:33 +0530

    fixed

--- backend/middlewares/authUser.js
+++ backend/middlewares/authUser.js
@@ -9,7 +9,7 @@ const authUser=async(req,res,next)=>{
     }
 
     const token_decode=jwt.verify(token,process.env.JWT_SECRET)
-    
+    req.body = req.body || {};
     req.body.userId=token_decode.id
     
     next()
