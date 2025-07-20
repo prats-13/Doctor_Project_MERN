93c2b14ba3b666eeaa6786d059680c07f0793d38
Pratham Lohia <121216418+prats-13@users.noreply.github.com>
2025-07-20 22:57:30 +0530

    created loadUserProfile data function

--- frontend/src/context/AppContextProvider.jsx
+++ frontend/src/context/AppContextProvider.jsx
@@ -8,6 +8,7 @@ const AppContextProvider= (props)=>{
   const backendUrl=import.meta.env.VITE_BACKEND_URL
   const [doctors,setDoctors]=useState([])
   const [token,setToken]=useState(localStorage.getItem('token')?localStorage.getItem('token'):false)
+  const [userData,setUserData]=useState(false)
 
   const getDoctorsData=async()=>{
     try{
@@ -22,18 +23,47 @@ const AppContextProvider= (props)=>{
       toast.error(error.message)
     }
   }
-  
+
+  const loadUserProfileData=async()=>{
+    try{
+      const {data}=await axios.get(backendUrl+'/api/user/get-profile',{headers:{token}})
+
+      if(data.success){
+        setUserData(data.userData)
+      }else{
+        toast.error(data.message)
+      }
+    }catch(error){
+      console.log(error)
+      toast.error(error.message)
+    }
+  }
+
   const value={
     doctors,
     currencySymbol,
     token,
     setToken,
     backendUrl,
+    userData,
+    setUserData,
+    loadUserProfileData
   }
 
   useEffect(()=>{
-    getDoctorsData()
+    getDoctorsData(),
+    loadUserProfileData
   })
+
+    useEffect(()=>{
+      if(token){
+        loadUserProfileData()
+      }else{
+        setUserData(false)
+      }
+  },
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  [token])
 
   return(
     <AppContext.Provider value={value}>
