4e80ee65f55ae5f36275e1a90ee3ff816a279571
Pratham Lohia <121216418+prats-13@users.noreply.github.com>
2025-07-20 21:40:47 +0530

    updated contexts to share

--- frontend/src/context/AppContextProvider.jsx
+++ frontend/src/context/AppContextProvider.jsx
@@ -7,11 +7,7 @@ const AppContextProvider= (props)=>{
   const currencySymbol='$'
   const backendUrl=import.meta.env.VITE_BACKEND_URL
   const [doctors,setDoctors]=useState([])
-
-  const value={
-    doctors,
-    currencySymbol,
-  }
+  const [token,setToken]=useState('')
 
   const getDoctorsData=async()=>{
     try{
@@ -27,6 +23,14 @@ const AppContextProvider= (props)=>{
     }
   }
   
+  const value={
+    doctors,
+    currencySymbol,
+    token,
+    setToken,
+    backendUrl,
+  }
+
   useEffect(()=>{
     getDoctorsData()
   })
