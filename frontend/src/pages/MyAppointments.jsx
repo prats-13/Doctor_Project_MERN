5c4cfc235a1a74f7d83eaa45781d8040cd5671fb
Pratham Lohia <121216418+prats-13@users.noreply.github.com>
2025-12-30 00:23:50 +0530

    typo

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -42,7 +42,7 @@ const MyAppointments = () => {
             <p className='text-zinc-700 font-medium mt-1'>Address</p>
             <p className='text-xs'>{item.docData.address.line1}</p>
             <p className='text-xs'>{item.docData.address.line2}</p>
-            <p className='text-xs mt-1'><span className='text-sm text-neutral-700 font-medium'>Date & Time:</span> {item.slotdate} | {item.slotTime}</p>
+            <p className='text-xs mt-1'><span className='text-sm text-neutral-700 font-medium'>Date & Time:</span> {item.slotDate} | {item.slotTime}</p>
           </div>
           <div></div>
           <div className='flex flex-col gap-2 justify-end'>
